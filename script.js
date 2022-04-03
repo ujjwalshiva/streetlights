01ec587198d356d5737873f5b553c9c03e366f05
Ujjwal Acharya <81429137+ujjwalshiva@users.noreply.github.com>
2022-04-03 15:53:40 +0530

    Image Upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let imgbb_api = "https://api.imgbb.com/1/upload?key=db8607dd64175f1c3527ff2101aba830&image="
+var url;
+
 const image_input = document.querySelector("#image_input");
 var uploaded_image;
 
@@ -7,7 +10,19 @@ image_input.addEventListener('change', function () {
         uploaded_image = reader.result;
         document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`;
     });
+    var form = new FormData();
+    form.append("image",this.files[0])
     reader.readAsDataURL(this.files[0]);
     console.log(this.files[0]);
+    fetch(imgbb_api, {
+        method: "POST",
+        body: form
+    }).then(e => e.json()).then(function (e) {
+        url = e.data.display_url;
+        console.log(e.data.display_url);
+        console.log(url);
+    })
 });
+
+
 
